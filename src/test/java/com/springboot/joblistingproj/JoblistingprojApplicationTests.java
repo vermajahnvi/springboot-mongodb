7ccc4e4b8d66dc50ef916e4f2b28d5bbf069d1b1
jahnvi.verma <vermajahnvi19@gmail.com>
2023-05-08 15:54:52 +0530

    initial commit

---
+++ src/test/java/com/springboot/joblistingproj/JoblistingprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.joblistingproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoblistingprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
